79beed8663098bfd96f92e2353b7de6e68c22525
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2025-08-25 15:57:50 +0300

    updated

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,10 +1,39 @@
-const JobsPage = () => {
+import { DataTable } from "./data-table"
+import {columns,Payment} from "./columns"
+import { Button } from "@/components/ui/button"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+  ]
+}
+
+const JobsPage = async () => {
+      const data = await getData()
       return (
-        <div>
-            <p>Merhaba ben İşler sayfası</p>
+      <Tabs defaultValue="job" className="container mx-auto py-3">
+        <div className="flex flex-row justify-between pb-3">
+          <TabsList>
+            <TabsTrigger value="job">Senin İşlerin</TabsTrigger>
+            <TabsTrigger value="all-job">Tüm İşler</TabsTrigger>
+            <TabsTrigger value="department-job">Departman Bazlı işler</TabsTrigger>
+          </TabsList>
+          <Button variant="default">Oluştur</Button>
         </div>
+        <TabsContent value="job"> <DataTable columns={columns} data={data}/></TabsContent>
+        <TabsContent value="all-job"> </TabsContent>
+        <TabsContent value="department-job"></TabsContent>
+      </Tabs>
     )
 }
 
-
 export default JobsPage
+
+
